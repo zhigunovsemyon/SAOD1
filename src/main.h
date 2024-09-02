19741a45987e9d9841f534e270870ec929c96172
Semyon <zhigunovsemen@gmail.com>
2024-09-02 20:49:26 +0300

    Добавил коды ошибок

--- src/main.h
+++ src/main.h
@@ -20,6 +20,12 @@
 #define L4KEY "physics" //Сортировка по четвёртому предмету
 #define L5KEY "philosophy" //Сортировка по пятому предмету
 
+//Коды ошибок
+#define ERR_FILENOTFOUND 2 //Не удалось открыть файл
+#define ERR_NOSEARCHPATTERN 3 //Пользователь не указал строку поиска
+#define ERR_NOSORTKEY 4 //Пользователь не указал параметр для сортировки
+#define ERR_OUTOFMEM -1 //Не удалось выделить память
+
 //Вывод сообщения об использовании программы
 void usage(void);
 
