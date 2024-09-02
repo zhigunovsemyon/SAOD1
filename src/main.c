51bb4eb36182e9b5d331c997e12b8ccb2bc797bc
Semyon <zhigunovsemen@gmail.com>
2024-09-02 14:45:54 +0300

    Создал прототип вывода сообщения об ошибке

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
 int main (const int argc, const char** argv) {
+    /*Если пользователь не указал аргументы функции, 
+     * ему выведется инструкция по использованию программы */
+    if (argc < 2) {
+        usage();
+        return EXIT_SUCCESS;
+    }
     puts(argv[argc - 1]);
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+void usage(void) {
+    printf("Здесь будет сообщение об ошибке\n");
 }
--- src/main.h
+++ src/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H 
 #define MAIN_H 
 
+#include <stdlib.h>
 #include <stdio.h>
+
+//Вывод сообщения об использовании программы
+void usage(void);
 
 #endif
