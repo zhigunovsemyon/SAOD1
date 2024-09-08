c971686a28640c4462cc3da760236449b009ccd0
Semyon <zhigunovsemen@gmail.com>
2024-09-08 19:06:28 +0300

    Перенёс обратную сортировку по словам в sort.c

--- src/sort.h
+++ src/sort.h
@@ -1,4 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <string.h>
+
+//Функция инвертированной сортировки по словам
+int invert_strcmp(const char *str1, const char *str2);
+
+
 #endif // !SORT_H
